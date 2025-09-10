ca9d75c7591c8ca67cbdc78744389ce0c960cc21
iury285c <ig285c@gmail.com>
2025-09-10 10:18:37 -0300

    add logica e um padrão de comunicação

--- src/main/java/br/com/fillter/FilterAutenticacao.java
+++ src/main/java/br/com/fillter/FilterAutenticacao.java
@@ -6,10 +6,15 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import br.com.jpautil.JPAUtil;
 
 
 @WebFilter
@@ -17,20 +22,30 @@ public class FilterAutenticacao implements Filter {
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		JPAUtil.getEntityManager();
 		
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		String usuarioLogado = (String) session.getAttribute("usuarioLogado");
+		String url = req.getServletPath();
+		if (!url.equalsIgnoreCase("index.jsf") && usuarioLogado == null ||
+			(usuarioLogado != null && usuarioLogado.trim().isEmpty())) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsf");
+			dispatcher.forward(request, response);
+			return;
+		}else {
+			chain.doFilter(request, response);	
+		}
 		
 	}
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
 		
 	}
 
