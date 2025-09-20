a4a09b7a239323449182d1be34533c79dada1b1f
iury285c <ig285c@gmail.com>
2025-09-20 01:52:39 -0300

    add metodo selectItem, para a criação de uma lista de estados

--- src/main/java/br/com/repository/IDaoPessoaImpl.java
+++ src/main/java/br/com/repository/IDaoPessoaImpl.java
@@ -1,28 +1,53 @@
 package br.com.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.component.UISelectItems;
+import javax.faces.model.SelectItem;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import br.com.entidade.Estados;
 import br.com.entidade.Pessoa;
-import br.com.jpautil.JPAUtil;
+
+
 
 public class IDaoPessoaImpl implements IDaoPessoa {
 
+	@Inject
+	private EntityManager entityManager;
+	
 	@Override
 	public Pessoa consultarUsuario(String login, String senha) {
 		
 		Pessoa pessoa = null;
 		
-		EntityManager entityManager = JPAUtil.getEntityManager();
+		
 		EntityTransaction entityTransaction = entityManager.getTransaction();
 		entityTransaction.begin();
 		
 		pessoa = (Pessoa) entityManager.createQuery("select p from Pessoa p where p.login = '" + login + "' and p.senha = '" + senha +"'").getSingleResult();
 		
 		entityTransaction.commit();
-		entityManager.close();
+		
 		
 		return pessoa;
+	}
+
+	@Override
+	public List<SelectItem> listaEstados() {
+		List<SelectItem> selectItems = new ArrayList<SelectItem>();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		List<Estados> estados = entityManager.createQuery("from Estados").getResultList();
+		
+		for (Estados estado : estados) {
+			selectItems.add(new SelectItem(estado, estado.getNome()));
+		}
+		return selectItems;
 	}
 
 }
