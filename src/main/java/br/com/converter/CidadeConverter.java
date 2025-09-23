3cc2fb99a5f742d003f0e0933b47667a273b2bea
iury285c <ig285c@gmail.com>
2025-09-23 10:10:17 -0300

    add cidadeconverter ao projeto

---
+++ src/main/java/br/com/converter/CidadeConverter.java
@@ -0,0 +1,38 @@
+package br.com.converter;
+
+import java.io.Serializable;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.entidade.Cidades;
+import br.com.jpautil.JPAUtil;
+
+@FacesConverter(forClass = Cidades.class)
+public class CidadeConverter implements Converter<Object>, Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String codigoCidade) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		Cidades cidades = (Cidades) entityManager.find(Cidades.class, Long.parseLong(codigoCidade));
+		
+		return cidades;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object cidades) {
+		
+		return ((Cidades) cidades).getId().toString();
+	}
+
+}
